11cad43edba222be972b2df210df7b50fb01dcd1
Artyom Dogil <artemdogil@gmail.com>
2022-09-24 15:27:30 +0300

    added delete action on view

--- force-app/main/default/lwc/sensorsTable/sensorsTable.js
+++ force-app/main/default/lwc/sensorsTable/sensorsTable.js
@@ -14,10 +14,52 @@ export default class SensorsTable extends LightningElement {
         }
     }
     columns = columns;
+    record = {};
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        switch (actionName) {
+            case 'delete':
+                this.deleteRow(row);
+                break;
+            default:
+        }
+    }
+
+    deleteRow(row) {
+        const { id } = row;
+        const index = this.findRowIndexById(id);
+        if (index !== -1) {
+            this.data = this.data
+                .slice(0, index)
+                .concat(this.data.slice(index + 1));
+        }
+    }
+
+    findRowIndexById(id) {
+        let ret = -1;
+        this.data.some((row, index) => {
+            if (row.id === id) {
+                ret = index;
+                return true;
+            }
+            return false;
+        });
+        return ret;
+    }
+
 }
+const actions = [
+    { label: 'Delete', name: 'delete' },
+];
 const columns = [
     { label: 'Name', fieldName: 'Name' },
     { label: 'Sensor model', fieldName: 'Sensor_model__c' },
     { label: 'Status', fieldName: 'Status__c' },
-    { label: 'Base Station', fieldName: 'Base_Station__r.Name' }
+    { label: 'Base Station', fieldName: 'Base_Station__c' },
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions },
+    },
 ];
